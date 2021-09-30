1011a2f286c131a787e301866883af6fb52598ce
porwaltz <38365041+porwaltz@users.noreply.github.com>
2021-09-30 05:37:41 -0300

    src again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
-import { AppComponent } from "docs/app/app.component";
-import { HeaderComponent } from "docs/app/components/header/header.component";
-import { BodyComponent } from "docs/app/components/body/body.component";
-import { FooterComponent } from "docs/app/components/footer/footer.component";
+import { AppComponent } from "src/app/app.component";
+import { HeaderComponent } from "src/app/components/header/header.component";
+import { BodyComponent } from "src/app/components/body/body.component";
+import { FooterComponent } from "src/app/components/footer/footer.component";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, BodyComponent, FooterComponent],
